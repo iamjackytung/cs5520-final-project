379a69334cf9b7d0b962da503bc0d0567510f335
victor <vwilm@outlook.com>
2023-03-31 14:46:25 -0700

    boilerplate

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button, lightColors, createTheme, ThemeProvider, Avatar } from '@rneui/themed';
-import Home from './screens/Home';
-import MyMentors from './screens/MyMentors';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  Button,
+  lightColors,
+  createTheme,
+  ThemeProvider,
+  Avatar,
+} from "@rneui/themed";
+import Home from "./screens/Home";
+import MyMentors from "./screens/MyMentors";
 
 // const theme = createTheme({
 //   // Colors
@@ -37,18 +43,16 @@ export default function App() {
           <Tab.Navigator
             screenOptions={{
               headerShown: false,
-            //   headerStyle: { backgroundColor: "green" },
-            //   headerTintColor: "white",
-            //   headerTitleStyle: { fontSize: 30 },
+              //   headerStyle: { backgroundColor: "green" },
+              //   headerTintColor: "white",
+              //   headerTitleStyle: { fontSize: 30 },
             }}
           >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="MyMentors" component={MyMentors} />
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="MyMentors" component={MyMentors} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
     </SafeAreaProvider>
   );
 }
-
-
--- screens/MyMentors.js
+++ screens/MyMentors.js
@@ -1,22 +1,104 @@
-import { StyleSheet, Text, View } from 'react-native';
-import React from 'react';
-import { Header } from '../components/Header';
-import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+import React, { useState } from "react";
+import {
+  View,
+  FlatList,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+import { ListItem, Avatar, Button, Icon } from "@rneui/themed";
 
-export default function MyMentors() {
+const data = require("../fakedb.json");
+
+const MentorScreen = () => {
+  const [searchText, setSearchText] = useState("");
+  const [viewStyle, setViewStyle] = useState("list");
+
+  const filteredUsers = data.users.filter((user) =>
+    `${user.firstName} ${user.lastName}`
+      .toLowerCase()
+      .includes(searchText.toLowerCase())
+  );
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity>
+      <ListItem bottomDivider>
+        <Avatar source={{ uri: item.profilePictureUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
+          <ListItem.Subtitle>{item.field}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    </TouchableOpacity>
+  );
+
+  const renderGridItem = ({ item }) => (
+    <TouchableOpacity style={styles.gridItem}>
+      <Avatar size="large" source={{ uri: item.profilePictureUrl }} />
+      <ListItem.Content>
+        <ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
+        <ListItem.Subtitle>{item.field}</ListItem.Subtitle>
+      </ListItem.Content>
+    </TouchableOpacity>
+  );
+
   return (
-    <>
-      <Header view="Home" title="My mentors" />  
-      <Text>MyMentors</Text>
-    </>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.searchInput}
+          onChangeText={setSearchText}
+          value={searchText}
+          placeholder="Search"
+        />
+        <Button
+          onPress={() => setViewStyle(viewStyle === "list" ? "grid" : "list")}
+          icon={
+            <Icon name={viewStyle === "list" ? "view-module" : "view-list"} />
+          }
+          type="clear"
+        />
+      </View>
+      <FlatList
+        data={filteredUsers}
+        renderItem={viewStyle === "list" ? renderItem : renderGridItem}
+        keyExtractor={(item) => item.userId}
+        numColumns={viewStyle === "list" ? 1 : 2}
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+  },
+  searchContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingHorizontal: 10,
+    marginTop: 10,
+  },
+  searchInput: {
+    flex: 1,
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    paddingHorizontal: 8,
+    marginRight: 10,
+  },
+  gridItem: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
   },
 });
+
+export default MentorScreen;
