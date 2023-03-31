aca9e311f9b0b388f8ea15022a7b5587fdb18c7d
Jacky Tung <jackyyet@usc.edu>
2023-03-31 00:37:08 -0700

    basic style changes to code

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
-import 'react-native-gesture-handler';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import "react-native-gesture-handler";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { Button, lightColors, createTheme, ThemeProvider, Avatar } from '@rneui/themed';
-import Home from './screens/Home';
-import MyMentors from './screens/MyMentors';
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  Button,
+  lightColors,
+  createTheme,
+  ThemeProvider,
+  Avatar,
+} from "@rneui/themed";
+import Home from "./screens/Home";
+import MyMentors from "./screens/MyMentors";
+// import SignUp from "./screens/SignUp";
+import SignUpInfo from "./screens/SignUpInfo";
+import SignUp from "./screens/SignUp";
 
 // const theme = createTheme({
 //   // Colors
@@ -31,24 +40,27 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
+    // <View>
+    //   <SignUpInfo></SignUpInfo>
+    // </View>
     <SafeAreaProvider>
       <ThemeProvider>
         <NavigationContainer>
           <Tab.Navigator
             screenOptions={{
               headerShown: false,
-            //   headerStyle: { backgroundColor: "green" },
-            //   headerTintColor: "white",
-            //   headerTitleStyle: { fontSize: 30 },
+              //   headerStyle: { backgroundColor: "green" },
+              //   headerTintColor: "white",
+              //   headerTitleStyle: { fontSize: 30 },
             }}
           >
-          <Tab.Screen name="Home" component={Home} />
-          <Tab.Screen name="MyMentors" component={MyMentors} />
+            <Tab.Screen name="Sign Up" component={SignUp} />
+            <Tab.Screen name="Home" component={Home} />
+            <Tab.Screen name="SignUp Info" component={SignUpInfo} />
+            <Tab.Screen name="MyMentors" component={MyMentors} />
           </Tab.Navigator>
         </NavigationContainer>
       </ThemeProvider>
     </SafeAreaProvider>
   );
 }
-
-
