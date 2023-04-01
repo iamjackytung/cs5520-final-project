3e26986d8785724f3a7cfb41e41416f6110fb9e6
victor <vwilm@outlook.com>
2023-03-31 20:01:47 -0700

    working code

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -1,8 +1,49 @@
-import { collection, addDoc } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs } from "firebase/firestore";
+import { auth, firestore } from "./firebase-setup";
+
 export async function writeToDB(goal) {
   try {
     await addDoc();
   } catch (err) {
     console.log(err);
+  }
+}
+
+export async function getMyMentors() {
+  try {
+    if (!auth.currentUser.uid) {
+      console.log("No user is signed in");
+      return null;
+    }
+    const usersCollection = collection(firestore, "users");
+    const userDocRef = doc(usersCollection, auth.currentUser.uid);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (!userDocSnap.exists()) {
+      console.log("User document does not exist.");
+      return;
+    }
+
+    const userMentors = userDocSnap.data().mentors;
+    const mentorDetailsList = [];
+
+    for (const mentorID of userMentors) {
+      const mentorDocRef = doc(usersCollection, mentorID);
+      const mentorDocSnap = await getDoc(mentorDocRef);
+
+      if (mentorDocSnap.exists()) {
+        const mentorData = mentorDocSnap.data();
+        mentorDetailsList.push({
+          id: mentorDocSnap.id,
+          firstName: mentorData.firstName,
+          lastName: mentorData.lastName,
+          profilePictureUrl: mentorData.profilePictureUrl,
+          jobTitle: mentorData.jobTitle,
+        });
+      }
+    }
+    return mentorDetailsList;
+  } catch (error) {
+    console.log("Error fetching mentors:", error);
   }
 }
