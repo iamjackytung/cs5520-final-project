d7a76793e2913e2f83a4826aa748bcfbde1808ee
victor <vwilm@outlook.com>
2023-03-31 22:07:36 -0700

    firebase helper for my mentors

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -26,3 +26,42 @@ export async function writeToDB(userData) {
 //       console.log("User added!");
 //     });
 // }
+
+export async function getMyMentors() {
+  try {
+    if (!auth.currentUser.uid) {
+      console.log("No user is signed in");
+      return null;
+    }
+    const usersCollection = collection(firestore, "users");
+    const userDocRef = doc(usersCollection, auth.currentUser.uid);
+    const userDocSnap = await getDoc(userDocRef);
+
+    if (!userDocSnap.exists()) {
+      console.log("User document does not exist.");
+      return;
+    }
+
+    const userMentors = userDocSnap.data().mentors;
+    const mentorDetailsList = [];
+
+    for (const mentorID of userMentors) {
+      const mentorDocRef = doc(usersCollection, mentorID);
+      const mentorDocSnap = await getDoc(mentorDocRef);
+
+      if (mentorDocSnap.exists()) {
+        const mentorData = mentorDocSnap.data();
+        mentorDetailsList.push({
+          id: mentorDocSnap.id,
+          firstName: mentorData.firstName,
+          lastName: mentorData.lastName,
+          profilePictureUrl: mentorData.profilePictureUrl,
+          jobTitle: mentorData.jobTitle,
+        });
+      }
+    }
+    return mentorDetailsList;
+  } catch (error) {
+    console.log("Error fetching mentors:", error);
+  }
+}
