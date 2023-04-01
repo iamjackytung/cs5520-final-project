22e7ff59b2ad83ddf98ebbb75f873f4fa9f68de4
Jacky Tung <jackyyet@usc.edu>
2023-03-31 21:22:37 -0700

    document can now be added to firebase

--- Firebase/firestoreHelper.js
+++ Firebase/firestoreHelper.js
@@ -1,8 +1,28 @@
-import { collection, addDoc } from "firebase/firestore";
-export async function writeToDB(goal) {
+import { collection, setDoc, doc } from "firebase/firestore";
+import { firestore } from "./firebase-setup";
+export async function writeToDB(userData) {
+  console.log(userData);
   try {
-    await addDoc();
+    // await addDoc(uid, ...signUpData);
+    // await addDoc(collection(...signUpData, uid)
+    console.log(userData.uid);
+    const newDoc = await setDoc(doc(firestore, "users", userData.uid), {
+      userData,
+    });
   } catch (err) {
     console.log(err);
   }
 }
+
+// export async function writeToDB(uid, signUpData) {
+//   firestore()
+//     .collection("users")
+//     .doc(uid)
+//     .set({
+//       name: "Ada Lovelace",
+//       age: 30,
+//     })
+//     .then(() => {
+//       console.log("User added!");
+//     });
+// }
