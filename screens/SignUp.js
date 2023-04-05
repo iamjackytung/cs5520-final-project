fa7ba3a5b76dc8695dc0953cb491546754fb50b4
Jacky Tung <jackyyet@usc.edu>
2023-04-04 18:48:30 -0700

    quick code cleanup

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -39,31 +39,6 @@ const TabSelector = ({ selected }) => {
   );
 };
 
-// export const UserTypeItem = (props) => {
-//   const { image, label, labelColor, selected, ...attributes } = props;
-//   return (
-//     <TouchableOpacity {...attributes}>
-//       <View
-//         style={[
-//           styles.userTypeItemContainer,
-//           selected && styles.userTypeItemContainerSelected,
-//         ]}
-//       >
-//         <Text style={[styles.userTypeLabel, { color: labelColor }]}>
-//           {label}
-//         </Text>
-//         <Image
-//           source={image}
-//           style={[
-//             styles.userTypeMugshot,
-//             selected && styles.userTypeMugshotSelected,
-//           ]}
-//         />
-//       </View>
-//     </TouchableOpacity>
-//   );
-// };
-
 const SignUp = ({ navigation }) => {
   const [isLoading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
@@ -124,7 +99,7 @@ const SignUp = ({ navigation }) => {
   };
 
   const loginHandler = async () => {
-    console.log("user id was " + auth.currentUser.uid);
+    // console.log("user id was " + auth.currentUser.uid);
     return signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         Alert.alert("Successfully Logged In");
