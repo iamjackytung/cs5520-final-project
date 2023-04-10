8e78d16a48632be4f37a49ce5ea4d627ad7e46b7
victor <vwilm@outlook.com>
2023-04-10 03:54:28 -0700

    mentor grid component

---
+++ components/MentorGridItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { TouchableOpacity, StyleSheet } from "react-native";
+import { Avatar, ListItem } from "@rneui/themed";
+
+const MentorGridItem = ({ item }) => {
+  return (
+    <TouchableOpacity style={styles.gridItem}>
+      <Avatar size="large" source={{ uri: item.profilePictureUrl }} />
+      <ListItem.Content>
+        <ListItem.Title>{`${item.firstName} ${item.lastName}`}</ListItem.Title>
+        <ListItem.Subtitle>{`${item.jobTitle}`}</ListItem.Subtitle>
+      </ListItem.Content>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    margin: 10,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "#ccc",
+    borderRadius: 5,
+  },
+});
+
+export default MentorGridItem;
