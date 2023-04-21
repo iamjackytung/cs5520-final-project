5c4945bb7d7ce326cbd1015d68c5d6d7302c4f9c
Guoao Wei <guoao.wei@outlook.com>
2023-04-20 23:41:14 -0700

    Change navigator structure

--- App.js
+++ App.js
@@ -3,27 +3,61 @@ import "react-native-gesture-handler";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useState, useEffect } from "react";
 import {
-  Button,
-  lightColors,
-  createTheme,
   ThemeProvider,
-  Avatar,
 } from "@rneui/themed";
 import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./Firebase/firebase-setup";
 import Home from "./screens/Home";
 import SignUpInfo from "./screens/SignUpInfo";
 import SignUp from "./screens/SignUp";
 import UserProfile from "./screens/UserProfile.js";
 import ClickedProfile from "./screens/ClickedProfile";
 import Booking from "./screens/Booking";
+import { PushTokenProvider } from "./contexts/PushTokenContext";
 
 const Stack = createNativeStackNavigator();
 
+const AuthStack = (
+  <>
+    <Stack.Screen name="SignUp" component={SignUp} options={{gestureEnabled: false}} />
+    <Stack.Screen name="SignUpInfo" component={SignUpInfo} />
+  </>
+)
+;
+
+const AppStack = (
+  <>
+    <Stack.Screen
+      name="Home"
+      component={Home}
+      options={{ gestureEnabled: false }}
+    />
+    <Stack.Screen name="UserProfile" component={UserProfile} />
+    <Stack.Screen name="ClickedProfile" component={ClickedProfile} />
+    <Stack.Screen name="Booking" component={Booking} />
+  </>
+);
+
 export default function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true);
+      } else {
+        setIsAuthenticated(false);
+      }
+    });
+  }, []);
+
   return (
     <SafeAreaProvider>
+      <PushTokenProvider>
       <ApplicationProvider {...eva} theme={eva.light}>
       <ThemeProvider>
         <NavigationContainer>
@@ -32,16 +66,12 @@ export default function App() {
               headerShown: false,
             }}
           >
-            <Stack.Screen name="SignUp" component={SignUp} options={{gestureEnabled: false}} />
-            <Stack.Screen name="Home" component={Home} options={{gestureEnabled: false}}/>
-            <Stack.Screen name="UserProfile" component={UserProfile} />
-            <Stack.Screen name="SignUpInfo" component={SignUpInfo} />
-            <Stack.Screen name="ClickedProfile" component={ClickedProfile} />
-            <Stack.Screen name="Booking" component={Booking} />
+            { isAuthenticated? AppStack : AuthStack }
           </Stack.Navigator>
         </NavigationContainer>
       </ThemeProvider>
       </ApplicationProvider>
+      </PushTokenProvider>
     </SafeAreaProvider>
   );
 }
