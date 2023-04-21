62c8796f39a7998a05c914cca45f59d60527fdfc
Jacky Tung <jackyyet@usc.edu>
2023-04-21 00:57:06 -0700

    make Header code more concise. Code reduced drastically using terinary operations

--- components/Header.js
+++ components/Header.js
@@ -1,22 +1,17 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   StyleSheet,
   View,
   Text,
-  Linking,
+  TouchableOpacity,
   StyleProp,
   TextStyle,
   ViewStyle,
 } from "react-native";
-import { useNavigation } from "@react-navigation/native";
-import { Header as HeaderRNE, HeaderProps, Icon, Avatar } from "@rneui/themed";
-import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation, useRoute } from "@react-navigation/native";
+import { Header as HeaderRNE, Icon, Avatar } from "@rneui/themed";
 import { auth, db } from "../Firebase/firebase-setup";
-// import { collection } from "firebase/firestore";
-// import { onValue, ref } from "firebase/database";
-import { doc, getDoc, onSnapshot } from "firebase/firestore";
-import { useState, useEffect } from "react";
-import { useRoute } from "@react-navigation/native";
+import { doc, onSnapshot } from "firebase/firestore";
 
 const Header = (props) => {
   const [photo, onChangePhoto] = useState(null);
@@ -31,103 +26,55 @@ const Header = (props) => {
       }
     );
 
-    return () => {
-      unsubscribe();
-    };
+    return () => unsubscribe();
   }, []);
 
-  const docsNavigate = () => {
-    // Linking.openURL(
-    //   `https://reactnativeelements.com/docs/components/${props.view}`
-    // );
-  };
   const leftAvatar = () => {
-    if (route.name == "UserProfile") {
+    if (route.name === "UserProfile") {
       return (
         <Avatar
-          size={"small"}
+          size="small"
           rounded
-<<<<<<< HEAD
           source={require("../assets/back.png")}
           onPress={() => navigation.goBack()}
-=======
-          source={require("../assets/emptyAvatar.png")}
-          onPress={() => navigation.navigate("UserProfile")}
->>>>>>> 7bfcf1e915e537890edf64d29391c07e7d6ec65c
         />
       );
-    } else {
-      if (photo == null) {
-        return (
-          <Avatar
-            size={"small"}
-            rounded
-            source={require("../assets/emptyAvatar.png")}
-            onPress={() => navigation.navigate("UserProfile")}
-          />
-        );
-      } else {
-        return (
-          <Avatar
-            size={"small"}
-            rounded
-            source={{ uri: photo }}
-            onPress={() => navigation.navigate("UserProfile")}
-          />
-        );
-      }
     }
-<<<<<<< HEAD
-=======
+
     return (
       <Avatar
-        size={"small"}
+        size="small"
         rounded
-        source={{ uri: photo }}
+        source={photo ? { uri: photo } : require("../assets/emptyAvatar.png")}
         onPress={() => navigation.navigate("UserProfile")}
       />
     );
->>>>>>> 7bfcf1e915e537890edf64d29391c07e7d6ec65c
   };
 
-  const playgroundNavigate = () => {
-    // Linking.openURL(`https://react-native-elements.js.org/#/${props.view}`);
-  };
   return (
     <HeaderRNE
-      // leftComponent={{
-      //   icon: 'menu',
-      //   color: '#fff',
-      //   onPress: navigation.openDrawer,
-      // }}
       leftComponent={leftAvatar()}
       leftContainerStyle={styles.headerLeft}
       rightComponent={
-        <>
-          <TouchableOpacity onPress={docsNavigate}>
+        <View style={styles.headerRight}>
+          <TouchableOpacity>
             <Icon name="description" color="white" />
           </TouchableOpacity>
-          <TouchableOpacity
-            style={{ marginLeft: 10 }}
-            onPress={playgroundNavigate}
-          >
+          <TouchableOpacity style={{ marginLeft: 10 }}>
             <Icon type="antdesign" name="rocket1" color="white" />
           </TouchableOpacity>
-        </>
+        </View>
       }
-      rightContainerStyle={styles.headerRight}
       centerComponent={{ text: props.title, style: styles.heading }}
     />
   );
 };
 
-const SubHeader = ({ title, containerStyle, textStyle }) => {
-  return (
-    <View style={[styles.headerContainer, containerStyle]}>
-      <Text style={[styles.heading, textStyle]}>{title}</Text>
-    </View>
-  );
-};
+const SubHeader = ({ title, containerStyle, textStyle }) => (
+  <View style={[styles.headerContainer, containerStyle]}>
+    <Text style={[styles.heading, textStyle]}>{title}</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   headerContainer: {
@@ -147,18 +94,12 @@ const styles = StyleSheet.create({
     display: "flex",
     flexDirection: "row",
     alignItems: "center",
-    // marginTop: 5,
   },
   headerLeft: {
     display: "flex",
     flexDirection: "row",
     alignContent: "center",
     marginLeft: 5,
-  },
-  subheaderText: {
-    color: "white",
-    fontSize: 16,
-    fontWeight: "bold",
   },
 });
 
